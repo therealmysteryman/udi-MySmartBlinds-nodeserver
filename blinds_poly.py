95d32f4978066d71405f4807310481cdd3638ef9
automationgeek <jf.tremblay@gmail.com>
2021-01-15 08:54:34 -0500

    Update blinds_poly.py

--- blinds_poly.py
+++ blinds_poly.py
@@ -69,7 +69,7 @@ class Controller(polyinterface.Controller):
         self.setDriver('ST', 1)
         self.reportDrivers()
         for node in self.nodes:
-            if  node != self.address and self.nodes[node].queryON == True :
+            if  node.address != self.address and self.nodes[node].queryON == True :
                 self.nodes[node].query()
 
     def longPoll(self):
